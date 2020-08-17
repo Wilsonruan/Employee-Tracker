5aa55406b6ff34d3ed680a5962775d0a19e9cc06
Wilson Ruan <wilsonruan23@gmail.com>
2020-08-17 14:42:15 -0400

    Remove addionital function

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: process.env.PORT || 3306,
     user: "root",
-    password: "Qwaszx92!",
+    password: "",
     database: "employee_db"
 });
 
